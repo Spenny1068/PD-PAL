efa44404e6ab17523d1e2730219b74790a482374
Zhong Jia Xue <zjxue@sfu.ca>
2019-10-18 20:44:55 -0700

    Adding the four main controller Swiftfiles

---
+++ PD_PAL/PD_PAL/RoutinesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RoutinesViewController.swift
+//  PD_PAL
+//
+//  Created by Zhong Jia Xue on 2019-10-18.
+//  Copyright © 2019 WareOne. All rights reserved.
+//
+
+import UIKit
+
+class RoutinesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
