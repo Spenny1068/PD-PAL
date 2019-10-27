a5638c76363db302bc8d91e0d13c392532af92f7
Spenny1068 <spncrlll838@gmail.com>
2019-10-26 20:39:49 -0700

    ffff

--- PD_PAL/PD_PAL/RoutinesViewController.swift
+++ PD_PAL/PD_PAL/RoutinesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  TrendViewController.swift
+//  RoutinesViewController.swift
 //  PD_PAL
 //
 //  Created by Zhong Jia Xue on 2019-10-18.
@@ -8,20 +8,15 @@
 
 import UIKit
 
-class TrendViewController: UIViewController {
+class RoutinesViewController: UIViewController {
 
-
-    @IBOutlet weak var Title_label: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = Setup.m_bgColor
         
-        // Do any additional setup after loading the view.
+       
     }
+    
 
-    
-    
     /*
     // MARK: - Navigation
 
