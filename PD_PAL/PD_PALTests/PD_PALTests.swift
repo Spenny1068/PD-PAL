f1e8aa21fb487fbc4c0ba635219a2a58351f9dc8
Zhong Jia Xue <zjxue@sfu.ca>
2019-10-26 23:20:34 -0700

    Adding First Test, Inserting into the DB

--- PD_PAL/PD_PALTests/PD_PALTests.swift
+++ PD_PAL/PD_PALTests/PD_PALTests.swift
@@ -11,17 +11,27 @@ import XCTest
 
 class PD_PALTests: XCTestCase {
 
+    let exDB = ExerciseDatabase()
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    //
+    func testDatabase_insertion() {
+        
+        var desc = "Elevate Weights while keepings arms still"
+        var cat = "Strength"
+        var body = "Arms"
+        var name = "Bicep Curls"
+        exDB.insert_exercise(Name: name , Desc: desc, Category: cat, Body: body)
+        
+        
     }
 
     func testPerformanceExample() {
