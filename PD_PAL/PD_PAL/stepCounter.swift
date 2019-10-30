b2af61d5157f5698dfd8769230e89ca8dc24e659
Julia <jka183@sfu.ca>
2019-10-29 20:31:31 -0700

    Implemented user permission, separated live updates and query data

--- PD_PAL/PD_PAL/stepCounter.swift
+++ PD_PAL/PD_PAL/stepCounter.swift
@@ -11,43 +11,94 @@
 /*Revision History
  
  -28/10/2019: Julia Kim
-    Created file, implemented CMPedometer
+    Created file, implemented CMPedometer, added "Privacy - Motion Usage Description" to Info.plist
+ -29/10/2019: Julia Kim
+    Implemented user permission, separated live updates and querying data history
+ 
+ still need to do for version 1:
+ test with real device + test cases
 */
 
 import CoreMotion
+import Foundation
 
 //create pedometer
 class StepCount{
     
-    let pedometer = CMPedometer() //instance of CMPedometer class
+    var pedometer = CMPedometer() //instance of CMPedometer class
+    var getHistory = false
+    var startDate = Date() //initialized to current date
+    var endDate = Date() //initialize to current date
+    
+    //see if specific date was selected
+    @IBAction func dateBtnSelected() -> Void{
+        getHistory = true
+        //need to finish this function for version 2
+    }
+    
+    func user_permission() -> Bool{
+        //wrapper func for CMAUthorizationStatus
+        //need to check for user permission using CMAuthorizationStatus
+        var authStat = false
+        switch CMPedometer.authorizationStatus()
+        {
+        case .denied: pedometer.stopUpdates()
+        case .authorized: authStat = true
+        default: break
+        }
+        
+        return authStat
+    }
     
     func track_steps(){
-        //check if step counting data is available
-        if CMPedometer.isStepCountingAvailable() {
-            //accumulate steps since the beginning date
-            //dates can be up to 7 days old
-            pedometer.startUpdates(from: Date()){ (data, error) in
-                //insert to DB to retain data older than 7 days
-                
+        //check if user authorized collecting data
+        if user_permission(){
+            //and check if step counting data is available
+            if CMPedometer.isStepCountingAvailable() {
+            
+                if getHistory //to be triggered by trends page later when viewing history
+                {
+                    //version 2
+                    //query step counter data
+                    query_steps()
+                }
+                else
+                {
+                    //get live updates
+                    live_updates() //only get live updates for version 1
+                }
             }
-            //pedometer.stopUpdates() //still need to figure this out
+        }
+        else
+        {
+            print("No permission to track steps")
         }
     }
     
-    /*
-    func user_permission(){
-        //wrapper func for CMAUthorizationStatus
-        //need to check for user permission using CMAuthorizationStatus
-        enum CMAuthorizationStatus: Int {
-            case notDetermined
-            case restricted
-            case denied
-            case authorized
+    func live_updates(){
+        //Date() gets the current date
+        pedometer.startUpdates(from: Date()) { pedometerData, error in
+                guard let pedData = pedometerData, error == nil else {return}
+            DispatchQueue.main.async{
+                print("\(pedData.numberOfSteps)")
+                //insert to user data DB instead of printing once DB is ready
+            }
+            }
+    }
+    
+    
+    func query_steps() {
+        //to used for version2
+       //query step data for specified dates
+        pedometer.queryPedometerData(from: startDate, to: endDate) { pedometerData, error in guard let pedData = pedometerData, error == nil else {print("Error getting the history")
+            return}
+            print("\(pedData)")
         }
         
-        let authStat = CMPedometer.authorizationStatus()
-        
-    }*/
+    }
+    
+    
+  
 }
 
 
