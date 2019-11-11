5afd57a831c45ea8e0c71850d9e3a2015732d4cc
Julia Kim <jka183@lib3000-icmac19.ucslabs.sfu.ca>
2019-11-11 14:48:46 -0800

    Adding radar graph using a diff lib and trying to implement scroll

--- PD_PAL/viewControllers/TrendViewController.swift
+++ PD_PAL/viewControllers/TrendViewController.swift
@@ -12,7 +12,7 @@
 // <November 8, 2019, Julia Kim, Getting counts for each category>
 
 import UIKit
-import Charts //import to allow creating graphs
+import RadarChart //import to allow creating radar graph
 
 class TrendViewController: UIViewController, UITableViewDataSource {
 
@@ -20,7 +20,8 @@ class TrendViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var Title_label: UILabel!
     @IBOutlet weak var trendTableView: UITableView!
     @IBOutlet weak var UpdateButton: UIButton!
-    @IBOutlet weak var radarChartView: RadarChartView!
+  
+    @IBOutlet weak var rChartView: RadarChartView!
     
     var strengthCounter = 0
     var flexCounter = 0
@@ -55,8 +56,14 @@ class TrendViewController: UIViewController, UITableViewDataSource {
         msg.text = "You're Doing Great!"
         msg.applyPageMsgDesign()
         self.view.addSubview(msg)
-      
         
+        generateRadarChart() //generate radar chart on load
+    }
+    
+    override func viewDidLayoutSubviews() {
+        rChartView?.prepareForDrawChart()
+        rChartView?.setNeedsLayout()
+        rChartView?.setNeedsDisplay()
     }
     
     // table View Material From https://www.youtube.com/watch?v=kCIQM7L-w4Y
@@ -92,19 +99,55 @@ class TrendViewController: UIViewController, UITableViewDataSource {
     @IBAction func Update(_ sender: UIButton) {
         exerciseData = global_UserData.Get_Exercises_all()
         self.trendTableView.reloadData()
-        //generateRadarChart() //need its own button
     }
 
     func generateRadarChart(){
-        //var dataEntries = exerciseCategoryCount()
+        let color = UIColor(red:0.282, green:0.541, blue:0.867, alpha:0.50)
+        let backgroundColor = UIColor(red:0.851, green:0.851, blue:0.941, alpha:1.00)
+        let xAxisColor = UIColor(red:0.396, green:0.769, blue:0.914, alpha:1.00)
+        let yAxisColor = UIColor(red:0.596, green:0.863, blue:0.945, alpha:1.50)
+        let fontColor = UIColor(red:0.259, green:0.365, blue:0.565, alpha:1.00)
         
+        rChartView?.data = self.exerciseCategoryCount()
+        rChartView?.labelTexts = ["Flexibility", "Cardio", "Balance", "Strength"]
+        rChartView?.numberOfVertexes = 4
+        rChartView?.numberTicks = 30
+        rChartView?.style = RadarChartStyle(color: color,
+                                        
+                                        backgroundColor: backgroundColor,
+                                        
+                                        xAxis: RadarChartStyle.Axis(
+                                            
+                                            colors: [xAxisColor],
+                                            
+                                            widths: [0.5, 0.5, 0.5, 0.5, 2.0]),
+                                        
+                                        yAxis: RadarChartStyle.Axis(
+                                            
+                                            colors: [yAxisColor],
+                                            
+                                            widths: [0.5]),
+                                        
+                                        label: RadarChartStyle.Label(fontName: "Helvetica",
+                                                                     
+                                                                     fontColor: fontColor,
+                                                                     
+                                                                     fontSize: 11,
+                                                                     
+                                                                     lineSpacing: 0,
+                                                                     
+                                                                     letterSpacing: 0,
+                                                                     
+                                                                     margin: 10))
+        
+        rChartView?.option = RadarChartOption()
     }
     
     //This function fetches exercises done by the user, identifies the category to count how many exercises are done in each category
     func exerciseCategoryCount() -> [Int]{
         let exerciseData = global_UserData.Get_Exercises_all()
         var categoryMatch = (" ", " ", " ", " ", " ")
-        var catCount = [0, 1, 2, 3]
+        var catCount = [0, 0, 0, 0]
         for entry in exerciseData{
             //get the category of the exercise done fetched from the DB
             categoryMatch = global_ExerciseData.read_exercise(NameOfExercise: entry.nameOfExercise)
@@ -113,8 +156,8 @@ class TrendViewController: UIViewController, UITableViewDataSource {
             if categoryMatch.1 as String == "Flexibility"
             {
                 flexCounter += 1
-                //print(categoryMatch.1)
-                //print(flexCounter)
+                print(categoryMatch.1)
+                print(flexCounter)
                 catCount[0] = flexCounter
             }
             else if categoryMatch.1 as String == "Cardio"
