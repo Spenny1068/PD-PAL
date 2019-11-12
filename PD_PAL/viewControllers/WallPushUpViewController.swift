95d31c2ea0b13def06c26fdd9774e7db085e783c
sslall <sslall@lib3000-icmac25.ucslabs.sfu.ca>
2019-11-12 15:41:08 -0800

    gif added

--- PD_PAL/viewControllers/WallPushUpViewController.swift
+++ PD_PAL/viewControllers/WallPushUpViewController.swift
@@ -110,6 +110,12 @@ class WallPushUpViewController: UIViewController {
             // timer label
             timerLabel.timerDesign()
             self.view.addSubview(timerLabel)
+            
+            // gif
+            guard let gif = UIImageView.fromGif(frame: CGRect(x: 0, y: 112, width: 375, height: 300), resourceName: "heel_stand") else { return }
+            view.addSubview(gif)
+            gif.startAnimating()
+
         }
     }
     
@@ -176,6 +182,28 @@ class WallPushUpViewController: UIViewController {
             stopButton.isHidden = true
             completedButton.isHidden = false
         }
+    }
+}
+
+extension UIImageView {
+    static func fromGif(frame: CGRect, resourceName: String) -> UIImageView? {
+        guard let path = Bundle.main.path(forResource: resourceName, ofType: "gif") else {
+            print("Gif does not exist at that path")
+            return nil
+        }
+        let url = URL(fileURLWithPath: path)
+        guard let gifData = try? Data(contentsOf: url),
+            let source =  CGImageSourceCreateWithData(gifData as CFData, nil) else { return nil }
+        var images = [UIImage]()
+        let imageCount = CGImageSourceGetCount(source)
+        for i in 0 ..< imageCount {
+            if let image = CGImageSourceCreateImageAtIndex(source, i, nil) {
+                images.append(UIImage(cgImage: image))
+            }
+        }
+        let gifImageView = UIImageView(frame: frame)
+        gifImageView.animationImages = images
+        return gifImageView
     }
 }
 
