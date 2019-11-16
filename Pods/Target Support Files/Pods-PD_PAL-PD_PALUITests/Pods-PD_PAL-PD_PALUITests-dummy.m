681f5c202cb8226bacce6018bd85a45193abc2f0
William Huong <william@Williams-iMac.local>
2019-11-16 10:09:45 -0800

    Preparing to merge dev-2 into branch

--- Pods/Target Support Files/Pods-PD_PAL-PD_PALUITests/Pods-PD_PAL-PD_PALUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PD_PAL_PD_PALUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PD_PAL_PD_PALUITests
-@end
--- Pods/Target Support Files/Pods-PD_PAL/Pods-PD_PAL-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PD_PAL : NSObject
-@end
-@implementation PodsDummy_Pods_PD_PAL
-@end
