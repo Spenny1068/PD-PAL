d6b3548c0289bd6e5537623ddafb1596224442f4
Spenny1068 <spncrlll838@gmail.com>
2019-11-29 14:14:24 -0800

    whoops, forgot to exlude the file from the repo that im trying to ignore

--- showDBfiles.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-declare -a files=("UserInfo.sqlite3" "StepCount.sqlite3" "exercises.sqlite3", "Routines.sqlite3", "UserExerciseData.sqlite3")
-
-clear
-for file in "${files[@]}"; do
-    path=$(find /Users/spenc/Library/Developer/CoreSimulator/Devices/ -name $file)
-    printf "$path"
-done
